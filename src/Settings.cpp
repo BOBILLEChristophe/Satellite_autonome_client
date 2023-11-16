58e2d251fbe49e93e63a9a07dd14e6990fea45fa
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-11-17 00:42:19 +0100

    Update Settings.cpp

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -11,17 +11,16 @@ bool Settings::WIFI_ON = true;
 bool Settings::DISCOVERY_ON = true;
 String Settings::ssid_str;
 String Settings::password_str;
-//String Settings::ssid = "Livebox-BC90";
 char Settings::ssid[30] = {};
 char Settings::password[30] = {};
 // uint8_t Settings::idNode = NO_ID;
-uint8_t Settings::nbLoco = 0;
+// uint8_t Settings::nbLoco = 0;
 bool Settings::isMainReady = false;
 Node *Settings::node = nullptr;
 // uint8_t Settings::gIdNode() { return Settings::idNode; }
-uint8_t Settings::gNbLoco() { return nbLoco; }
+// uint8_t Settings::gNbLoco() { return nbLoco; }
 // void Settings::sIdNode(uint8_t idNode) { Settings::idNode = idNode; }
-void Settings::sNbLoco(uint8_t nbLoco) { Settings::nbLoco = nbLoco; }
+// void Settings::sNbLoco(uint8_t nbLoco) { Settings::nbLoco = nbLoco; }
 void Settings::sMainReady(bool val) { Settings::isMainReady = val; }
 bool Settings::discoveryOn() { return DISCOVERY_ON; }
 void Settings::discoveryOn(bool val) { Settings::DISCOVERY_ON = val; }
@@ -35,6 +34,11 @@ void Settings::wifiOn(bool val) { Settings::WIFI_ON = val; }
 void Settings::setup(Node *nd)
 {
   node = nd;
+  if (!SPIFFS.begin(true))
+  {
+    debug.printf("An Error has occurred while mounting SPIFFS\n\n");
+    return;
+  }
   readFile();
 }
 
@@ -44,13 +48,16 @@ bool Settings::begin()
   do
   {
     CanMsg::sendMsg(0, node->ID(), 254, 0xB2);
+    if (!isMainReady)
+    {
 #ifdef DEBUG
-    debug.printf("Attente de reponse en provenance de la carte Main.\n");
+      debug.printf("Attente de reponse en provenance de la carte Main.\n");
 #endif
-    delay(1000);
+      delay(1000);
+    }
   } while (!isMainReady);
 
-  --- Identifiant du Node
+  //--- Identifiant du Node
   while (node->ID() == NO_ID) // L'identifiant n'est pas en mémoire
   {
     //--- Requete identifiant
@@ -59,28 +66,6 @@ bool Settings::begin()
   }
 
   writeFile();
-
-  //--- Requete nbre locos
-  // CanMsg::sendMsg(0, node->ID(), 254, 0xB6);
-  // debug.printf("Nombre de locos : %d\n", nbLoco);
-
-  // for (byte i = 0; i < nbLoco; i++)
-  // {
-  //   debug.printf("Infos loco %d.\n", i);
-  //   byte compt = 0;
-  //   // while (0 == node->rfid->locoTag[i].address0)
-  //   // {
-  //   CanMsg::sendMsg(0, node->ID(), 1, 0xB8, i);
-  //      delay(1000);
-  //   //   if (compt == 10)
-  //   //   {
-  //   //     debug.println("Impossible de charger la liste des locomotives.");
-  //   //     break;
-  //   //   }
-  //   //   compt++;
-  //   // }
-  //   compt = 0;
-  // }
 
 #ifdef DEBUG
   debug.printf("End settings\n");
@@ -96,43 +81,27 @@ bool Settings::begin()
 
 void Settings::readFile()
 {
-  if (!SPIFFS.begin(true))
-  {
-#ifdef DEBUG
-    debug.printf("An Error has occurred while mounting SPIFFS\n\n");
-#endif
-    return;
-  }
   File file = SPIFFS.open("/settings.json", "r");
   if (!file)
   {
-#ifdef DEBUG // File not found | le fichier de test n'existe pas
     debug.println("Failed to open settings.json\n\n");
-#endif
     return;
   }
   else
   {
-#ifdef DEBUG
     debug.printf("\nInformations du fichier \"settings.json\" : \n\n");
-#endif
-
-    DynamicJsonDocument doc(2048);
+    DynamicJsonDocument doc(4 * 1024);
     DeserializationError error = deserializeJson(doc, file);
     delay(100);
     if (error)
-    {
-#ifdef DEBUG
       debug.printf("Failed to read file, using default configuration\n\n");
-#endif
-    }
     else
     {
       // ---
       node->ID(doc["idNode"] | NO_ID);
-#ifdef DEBUG
+
       debug.printf("- ID node : %d\n", node->ID());
-#endif
+
       Discovery::comptAig(doc["comptAig"]);
       node->masqueAig(doc["masqueAig"]);
       WIFI_ON = doc["wifi_on"];
@@ -141,11 +110,6 @@ void Settings::readFile()
       password_str = doc["password"].as<String>();
       strcpy(ssid, ssid_str.c_str());
       strcpy(password, password_str.c_str());
-
-#ifdef DEBUG
-      debug.printf("- ssid : %s\n", ssid);
-      debug.printf("- password : %s\n", password);
-#endif
 
       // Nœuds
       const char *index[] = {"p00", "p01", "p10", "p11", "m00", "m01", "m10", "m11"};
@@ -156,23 +120,21 @@ void Settings::readFile()
           if (node->nodeP[i] == nullptr)
             node->nodeP[i] = new NodePeriph;
           node->nodeP[i]->ID(doc[index[i]]);
-#ifdef DEBUG
           debug.printf("- node->nodeP[%s]->id : %d\n", index[i], node->nodeP[i]->ID());
-#endif
         }
-#ifdef DEBUG
         else
           debug.printf("- node->nodeP[%s]->id : NULL\n", index[i]);
-#endif
       }
       debug.printf("---------------------------------\n");
 
       // Aiguilles
       for (byte i = 0; i < aigSize; i++)
       {
+        // debug.printf("valeur de aig %d : %s%c\n", i, doc["aig" + String(i)]);
         if (doc["aig" + String(i)] != "null")
         {
-          // node->aig[i] = new Aig;
+          if (node->aig[i] == nullptr)
+            node->aig[i] = new Aig;
           node->aig[i]->ID(doc["aig" + String(i) + "id"]);
           node->aig[i]->posDroit(doc["aig" + String(i) + "posDroit"]);
           node->aig[i]->posDevie(doc["aig" + String(i) + "posDevie"]);
@@ -183,9 +145,7 @@ void Settings::readFile()
           debug.printf("- Creation de l'aiguille %d\n", i);
         }
       }
-#ifdef DEBUG
       debug.printf("---------------------------------\n");
-#endif
     }
     file.close();
     // SPIFFS.end();
@@ -198,13 +158,13 @@ void Settings::readFile()
 
 void Settings::writeFile()
 {
-  if (!SPIFFS.begin(true))
-  {
-#ifdef DEBUG
-    debug.println("An Error has occurred while mounting SPIFFS\n\n");
-#endif
-    return;
-  }
+  //   if (!SPIFFS.begin(true))
+  //   {
+  // #ifdef DEBUG
+  //     debug.println("An Error has occurred while mounting SPIFFS\n\n");
+  // #endif
+  //     return;
+  //   }
   File file = SPIFFS.open("/settings.json", "w");
   if (!file)
   {
@@ -215,7 +175,7 @@ void Settings::writeFile()
   }
   else
   {
-    DynamicJsonDocument doc(2048);
+    DynamicJsonDocument doc(4 * 1024);
 
     doc["idNode"] = node->ID();
     doc["comptAig"] = Discovery::comptAig();
