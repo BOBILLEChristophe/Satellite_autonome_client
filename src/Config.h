ec043d3824205e77fe6c270ffede5bd34920e2c8
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-11-17 22:24:54 +0100

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -70,8 +70,8 @@ const uint8_t sensorSize = 2;
 #endif
 
 /* ----- Sensors ------------------*/
-#define CAPT_PONCT_ANTIHOR_PIN GPIO_NUM_33
-#define CAPT_PONCT_HORAIRE_PIN GPIO_NUM_15
+#define CAPT_PONCT_ANTIHOR_PIN GPIO_NUM_15
+#define CAPT_PONCT_HORAIRE_PIN GPIO_NUM_33
 #define CAPT_PONCT_TEMPO 50UL
 
 /* ----- Registres a decalage ------*/
