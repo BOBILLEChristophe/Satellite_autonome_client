9bb857fba6b87fd83a7534dd2f8d3d25c6eb667a
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-11-17 22:24:46 +0100

    Update CanConfig.h

--- src/CanConfig.h
+++ src/CanConfig.h
@@ -10,13 +10,13 @@ CanConfig.h
 
                   1	2	3	4	5	6	7	8	9	10 11	12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29
 
-Priorité	        x	x
-ID expediteur			    x	x	x	x	x	x	x	x
-ID destinataire											  x	x	x	x	x	x	x	x
-Fonction											        								x	x	x	x	x	x	x	x
-Libre		  				                																		0	0	0
+Priorité	      x	x
+ID expediteur			x	x	x	x	x	x	x	x
+ID destinataire											x	x	x	x	x	x	x	x
+Fonction											        							x	x	x	x	x	x	x	x
+Libre		  				                																			0	0	0
                   0	0	0	0	0	0	0	0	0	0									0	0	0	0	0	0	0	0	0	0	0
-Masque								                0	0	0	0	0	0	0	0
+Masque								                	0	0	0	0	0	0	0	0
                   1	1	1	1	1	1	1	1	1	1	0	0	0	0	0	0	0	0	1	1	1	1	1	1	1	1	1	1	1 = 0x1FF807FF
 
 */
