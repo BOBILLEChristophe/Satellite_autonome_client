df13fe72ed55be6eeb1b35809776e6376e036cf4
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-11-19 23:35:51 +0100

    Update Loco.cpp

--- src/Loco.cpp
+++ src/Loco.cpp
@@ -14,8 +14,8 @@ void Loco::address(uint16_t address) {m_address = address;}
 uint16_t Loco::address() {return m_address;}
 void Loco::sens(bool sens) {m_sens = sens;}
 bool Loco::sens() {return m_sens;}
-void Loco::comptCmd0(uint8_t x) {m_comptCmd0 = x;}
-uint8_t Loco::comptCmd0() {return m_comptCmd0;}
-void Loco::comptCmd30(uint8_t x) {m_comptCmd30 = x;}
-uint8_t Loco::comptCmd30() {return m_comptCmd30;}
+// void Loco::comptCmd0(uint8_t x) {m_comptCmd0 = x;}
+// uint8_t Loco::comptCmd0() {return m_comptCmd0;}
+// void Loco::comptCmd30(uint8_t x) {m_comptCmd30 = x;}
+// uint8_t Loco::comptCmd30() {return m_comptCmd30;}
 
