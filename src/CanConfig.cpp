fdad221d563ae97c76dc9ce278fc1f8277693626
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-11-19 23:28:45 +0100

    Update CanConfig.cpp

--- src/CanConfig.cpp
+++ src/CanConfig.cpp
@@ -9,7 +9,7 @@
 
 void CanConfig::setup(const byte thisID, const bool discovery_on)
 {
-    debug.println("Configure ESP32 CAN");
+    debug.printf("[CanConfig %d] : Configure ESP32 CAN", __LINE__);
     ACAN_ESP32_Settings settings(CAN_BITRATE);
     settings.mRxPin = CAN_RX;
     settings.mTxPin = CAN_TX;
@@ -25,14 +25,14 @@ void CanConfig::setup(const byte thisID, const bool discovery_on)
         const ACAN_ESP32_Filter filter = ACAN_ESP32_Filter::singleExtendedFilter(
             ACAN_ESP32_Filter::dataAndRemote, thisID << 11, 0x1FF807FF);
         errorCode = ACAN_ESP32::can.begin(settings, filter);
-        debug.print("Can config filter\n");
+        debug.printf("[CanConfig %d] : config filter\n", __LINE__);
     }
 
     if (errorCode == 0)
-        debug.println("Can configuration OK !\n");
+        debug.printf("[CanConfig %d] : configuration OK !\n", __LINE__);
     else
     {
-        debug.printf("Can configuration error 0x%x\n", errorCode);
+        debug.printf("[CanConfig %d] : configuration error 0x%x\n", __LINE__, errorCode);
         delay(1000);
         return;
     }
