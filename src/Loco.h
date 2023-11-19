5ce7b1d3873e268b9e8d8059802c816fa4d73ce2
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-11-19 23:36:11 +0100

    Update Loco.h

--- src/Loco.h
+++ src/Loco.h
@@ -16,8 +16,6 @@ class Loco
 private:
   uint16_t m_address;
   bool m_sens; // 0 sens horaire - 1 sens anti horaire
-  uint8_t m_comptCmd0;
-  uint8_t m_comptCmd30;
 
 public:
   Loco(); // Constructor
@@ -25,10 +23,9 @@ public:
   uint16_t address();
   void sens(bool);
   bool sens();
-  void comptCmd0(uint8_t);
-  uint8_t comptCmd0();
-  void comptCmd30(uint8_t);
-  uint8_t comptCmd30();
+
+  uint8_t m_comptCmd0;
+  uint8_t m_comptCmd30;
 };
 
 #endif
