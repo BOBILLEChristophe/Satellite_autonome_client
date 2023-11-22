e236682994a5471fce7c0b38fd4e1bb56e763a26
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-11-22 19:40:47 +0100

    Update CanConfig.cpp

--- src/CanConfig.cpp
+++ src/CanConfig.cpp
@@ -15,22 +15,22 @@ void CanConfig::setup(const byte thisID, const bool discovery_on)
     settings.mTxPin = CAN_TX;
     uint32_t errorCode;
 
-    if (discovery_on)
-    {
-           // Si mode discovery, pas de filtre
+    // if (discovery_on)
+    // {
+    //        // Si mode discovery, pas de filtre
         errorCode = ACAN_ESP32::can.begin(settings);
         debug.printf("[CanConfig %d] : config without filter\n", __LINE__);
-    }
+    // }
      
-    else
-    {
-        // En fonctionnement, on active le filtre pour ne recevoir que les seuls messages
-        // qui concernent ce satellite
-        const ACAN_ESP32_Filter filter = ACAN_ESP32_Filter::singleExtendedFilter(
-            ACAN_ESP32_Filter::dataAndRemote, thisID << 11, 0x1FF807FF);
-        errorCode = ACAN_ESP32::can.begin(settings, filter);
-        debug.printf("[CanConfig %d] : config filter\n", __LINE__);
-    }
+    // else
+    // {
+    //     // En fonctionnement, on active le filtre pour ne recevoir que les seuls messages
+    //     // qui concernent ce satellite
+    //     const ACAN_ESP32_Filter filter = ACAN_ESP32_Filter::singleExtendedFilter(
+    //         ACAN_ESP32_Filter::dataAndRemote, thisID << 11, 0x1FF807FF);
+    //     errorCode = ACAN_ESP32::can.begin(settings, filter);
+    //     debug.printf("[CanConfig %d] : config filter\n", __LINE__);
+    // }
 
     if (errorCode == 0)
         debug.printf("[CanConfig %d] : configuration OK !\n", __LINE__);
